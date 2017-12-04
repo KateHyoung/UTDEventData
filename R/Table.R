1c7e9604263b4fd55c82b27c2f180611f9b5d1dd
Kate H. Kim <kate0550@gmail.com>
2017-12-04 14:18:26 -0600

    Reference class updating

---
+++ R/Table.R
@@ -0,0 +1,45 @@
+#' Extracting event data from the UTD real-time Event database with referenc classes
+#' @description This is the main function to extract subdata from the UTD Event data server by country names and time ranges.
+#'              The API key is required. Please contact the developer (\url{https://github.com/Sayeedsalam/spec-event-data-server}) to get the API key.
+#'
+#' @return Real time event data
+#' @export
+#' @examples Table$pullData(api_key=" ", table_name="Phoenix", country=list("USA","MEX","SYR","CHN"),
+#'  start="20171101", end="20171112")
+#' @param api_key The UTD API key provided by the developer
+#' @param table_name The name of data table you want to have.
+#' @param country List of countries you want to look for with ISO ALPHA-3 Code.
+#' @param start The "YYYYMMDD" format of the first date of a data set.
+#' @param end The "YYYYMMDD" format of the end date of a data set.
+
+
+Table <- setRefClass("Table",
+  fields = list (api_key = "character"),
+  methods = list(
+    pullData = function(table_name, country, start, end) {
+      if (table_name=="Phoenix") {
+        country_constraint = list('country_code'= list('$in'= country))
+
+        date_constraint = list('date8'=list('$gte'=start,'$lte'=end))
+
+        all_constraints = list(country_constraint, date_constraint)
+        query = list('$and'=all_constraints)
+
+        # Convert the data structure into a string
+        # The gsub removes the backslashes, but they get visually re-added when printing to console
+        query_string = gsub("\\", '', rjson::toJSON(query), fixed=TRUE)
+
+        # Request data from API
+        url <- 'http://149.165.156.33:5002/api/data?api_key='
+        url_submit <- paste(url, api_key,'&query=', query_string, sep='')
+
+        # getting data from url formatting
+        retrieved_data <- readLines(url_submit, warn=FALSE)
+        parsed_data <- jsonlite::fromJSON(retrieved_data)$data
+        return(parsed_data)
+      }
+      else {print("Not available now")}
+    }
+  ))
+
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# UTDEventData" 
+# UTDEventData 
+
+This is the R package with which a user can extract the UTD Event Data with the contry and time ranges.
+
+## Installation
+
+```
+library(devtools)
+install_github("UTDEventData")
+```
+
+##Authors
+Dr. Patrick T. Brandt <pbrandt@utdallas.edu>  
+Dr. Vito D'Orazio <dorazio@utdallas.edu>  
+Micheal J. Shoemate <michael.shoemate@utdallas.edu>  
+Jared Looper <jrl140030@utdallas.edu>  
+Hyoungah (Kate) Kim <hyoungah.kim@utdallas.edu>  
+
+## License
